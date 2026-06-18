1e898aad8a55b3c08d364fc3f0c229803f54e5ac
Onlinebusiness <onlinebusinessfy@gmail.com>
2026-06-18 01:04:28 -0700

    18 de junio del 2026, se termino la actividad del tasklist en su totalidad con la funcion de eliminar tarea y acomodarlas en orden de prioridad ya sea subiendolas o bajandolas. Todo esto con sus respectivos iconos y con la documentacion proporcionada por el maestro

--- tasklist/miApp/src/app/pages/home/home.page.ts
+++ tasklist/miApp/src/app/pages/home/home.page.ts
@@ -1,22 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonInput, IonList, IonIcon} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonInput, IonList, IonIcon, IonItemSliding, IonItemOptions, IonItemOption, IonReorder, IonReorderGroup } from '@ionic/angular/standalone';
 import { Task } from '../../models/task.models';
+import { inject, Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular/standalone';
+import { Preferences } from '@capacitor/preferences';
+import { ReorderEndCustomEvent } from '@ionic/angular';
 import { addIcons } from 'ionicons';
-import { addOutline } from 'ionicons/icons';
-import { addCircleOutline as addCircleOutline } from 'ionicons/icons';
+import { addCircleOutline, trashOutline } from 'ionicons/icons';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class Alert {
+  
+  private alertController: AlertController = inject(AlertController);
+
+  async showAlert(headerText:string, messageText:string) {
+    const alert = await this.alertController.create({
+      header: headerText,
+      message: messageText,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async confirmAlert (
+    header: string,
+    message: string,
+    funcionOK: Function,
+    cancelText: string = 'Cancelar',
+    confirmText: string = 'Aceptar'
+    ){
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: [
+        {
+          text: cancelText,
+          role: 'cancel',
+        },
+        {
+          text: confirmText,
+          role: 'confirm',
+          handler: () => {
+            funcionOK();
+          }
+        }
+      ],
+    });
+    
+    await alert.present();
+  }
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonInput, IonList, IonIcon, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonInput, IonList, IonIcon, IonItemSliding, IonItemOptions, IonItemOption, IonReorder, IonReorderGroup, CommonModule, FormsModule]
 })
-export class HomePage implements OnInit {
 
+export class HomePage implements OnInit {
+  
   newTaskStr: string = '';
+  private readonly KEY_TASK = 'local_key_task';
 
   tasks: Task[] = [
     {
@@ -35,8 +87,13 @@ export class HomePage implements OnInit {
     }
   ];
 
+  public alertService: Alert = inject(Alert);
+
   constructor() { 
-    addIcons({addCircleOutline});
+    addIcons({
+    addCircleOutline,
+    trashOutline
+    });
     console.log(this.tasks);
   }
 
@@ -72,9 +129,57 @@ export class HomePage implements OnInit {
       completed: false,
       priority: 'Medium'
     }
+
+    this.alertService.showAlert('Éxito', 'Tarea agregada');
     this.tasks.push(newTask);
+    this.saveTaskOnLocal();
     this.newTaskStr = '';
     console.log(this.tasks);
   }
 
+  confirmDelete(task: Task) {
+    this.alertService.confirmAlert(
+      'Aviso',
+      `Deseas borrar la tarea "${task.title}"?`,
+      () => this.deleteTask(task),
+      'NO',
+      'SI'
+    );
+  }
+
+  deleteTask(task: Task) {
+  const index = this.tasks.findIndex(t => t.id === task.id);
+
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+      this.saveTaskOnLocal();
+    }
+  }
+
+  actualizarPosiciones(event:ReorderEndCustomEvent) {
+    console.log("El arreglo antes del cambio: ", this.tasks);
+    this.tasks = event.detail.complete(this.tasks);
+    this.saveTaskOnLocal();
+    console.log("El arreglo después del cambio: ", this.tasks);
+  }
+
+  async ionViewWillEnter() {
+    const taskPrefences = await Preferences.get({ key: this.KEY_TASK });
+
+    if (taskPrefences.value) {
+      const tasks = JSON.parse(taskPrefences.value);
+      if (Array.isArray(tasks)) {
+        this.tasks = tasks;
+      }
+    }
+  }
+
+  saveTaskOnLocal(){
+    Preferences.set(
+      {
+      key: this.KEY_TASK,
+      value: JSON.stringify(this.tasks)
+      }
+    );
+  }
 }
