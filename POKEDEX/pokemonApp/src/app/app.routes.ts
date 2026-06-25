8c31e0ec49057568a969b9eceb2278094d74bc79
Onlinebusiness <onlinebusinessfy@gmail.com>
2026-06-24 22:30:26 -0700

    24 de junio del 2026, el día de hoy complete el avance numero 1 de la documentacion de la pokedex. Se creo el proyecto en ionic, se agregaron la lista de pokemones, y los detalles de ellos. Ademas, se borró la página de home y actualizarion las app.routes.ts para posteriormente crear el servicio pokemon. Se estableció la nueva ruta y finalmente en postman con unos endpoints se obtuvieron informacion del pokemon, busqueda por id y una lista pokemon.

---
+++ POKEDEX/pokemonApp/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'list-pokemons',
+    loadComponent: () => import('./list-pokemons/list-pokemons.page').then( m => m.ListPokemonsPage)
+  },
+  {
+    path: 'detail-pokemon/:id',
+    loadComponent: () => import('./detail-pokemon/detail-pokemon.page').then( m => m.DetailPokemonPage)
+  },
+];
